f07b01e75938be048758f06fb886416824c7bb68
Carlos Rodriguez <carlos@s8f.org>
2016-04-26 12:39:43 -0700

    use export

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import bundler from '@terraeclipse/bundler'
 import bundle from './bundle'
 
-const app = bundler(bundle)
-let RootComponent = app.get('components.App')
-app.get('utils.render')(RootComponent)
+const {components, utils} = bundler(bundle).export()
+utils.render(components.App)
